4627d483aeb9972d15139e1f07b07f3ba2f82f34
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2023-03-01 08:34:16 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+require_once 'Telegram.php';
+function getToken()
+{
+
+
+    $client = new Client(['verify' => false]);
+    $options = [
+        'multipart' => [
+            [
+                'name' => 'login',
+                'contents' => $_ENV['HEMIS_LOGIN']
+            ],
+            [
+                'name' => 'password',
+                'contents' => $_ENV['HEMIS_PASSWORD']
+            ]
+        ]];
+    $request = new Request('POST', 'https://student.ubtuit.uz/rest/v1/auth/login');
+    $res = $client->sendAsync($request, $options)->wait();
+//    file_put_contents('token.txt',$res->getBody()->data->token);
+return $res->getBody()->data->token;
+}
+
+try {
+    echo getToken();
+}catch (Exception $e){
+    echo $e->getMessage();
+}
+$telegram=new Telegram($_ENV['TELEGRAM_BOT_TOKEN']);
+$chat_id=$telegram->ChatID();
+$telegram->sendMessage([
+    'chat_id'=>$chat_id,
+    'text'=>'Hello'
+]);
