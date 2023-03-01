8548dcf73c77a91f530f543fbb8c614f6d1902aa
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2023-03-01 17:08:14 +0500

    demo cron job for heroku

--- demo_scheduler.php
+++ demo_scheduler.php
@@ -5,3 +5,5 @@ require_once 'Telegram.php';
 $telegram=new Telegram($_ENV['TELEGRAM_BOT_TOKEN']);
 $chat_id=1490424185;
 $text="Bu matn".date('d-m-Y-H-i-s')." da yuborildi";
+$content = ['chat_id' => $chat_id, 'text' => $text];
+$telegram->sendMessage($content);
