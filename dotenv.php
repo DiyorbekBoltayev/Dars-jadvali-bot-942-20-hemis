a06ee7f4b898a23cb652bba73dc20fdd664580b0
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2023-03-02 07:44:56 +0500

    working with db

--- dotenv.php
+++ dotenv.php
@@ -2,6 +2,10 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+if (getenv('APP_ENV') === 'production') {
+    // Load environment variables from Heroku Config Vars
+} else {
+    // Load environment variables from .env file using Dotenv
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
