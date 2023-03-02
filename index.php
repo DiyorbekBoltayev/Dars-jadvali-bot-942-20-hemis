9ad888545165a3a6f89ac16ba99e3a4b8541d947
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2023-03-02 07:28:42 +0500

    working with db

--- index.php
+++ index.php
@@ -80,7 +80,10 @@ try {
 
 
     }
-ini_set('display_errors', 1);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     function getToken()
     {
 
