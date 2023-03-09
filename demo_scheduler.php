cd8d7dc696476388328c7f3162f129a015cd6b01
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2023-03-09 20:46:12 +0500

    testing cron-job

--- demo_scheduler.php
+++ demo_scheduler.php
@@ -4,6 +4,13 @@ require 'vendor/autoload.php';
 require_once 'Telegram.php';
 $telegram=new Telegram($_ENV['TELEGRAM_BOT_TOKEN']);
 $chat_id=1490424185;
-$text="Bu matn".date('d-m-Y-H-i-s')." da yuborildi";
-$content = ['chat_id' => $chat_id, 'text' => $text];
-$telegram->sendMessage($content);
+$message="Bu bot juda foydali bo'ldi";
+$message = $telegram->sendMessage([
+    'chat_id' => $chat_id,
+    'text' => $message,
+]);
+
+$telegram->pinChatMessage([
+    'chat_id' => $chat_id,
+    'message_id' => $message->getMessageId(),
+]);
